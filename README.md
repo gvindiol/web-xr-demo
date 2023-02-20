e92d9e3e0bbb95fe40f3fe3f8bfdf211b35a955e
Giovanni Vindiola <gvindiol@ucsd.edu>
2023-02-20 11:13:59 -0800

    Added AR/VR Buttons

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ https://sbcode.net/threejs/introduction/
 
 https://developer.mozilla.org/en-US/docs/Web/API/WebXR_Device_API/Fundamentals
 
+https://codingxr.com/articles/getting-started-with-webxr-and-threejs/
+
 https://discoverthreejs.com/book/introduction/get-threejs/
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 // Creating the scene
@@ -6,19 +9,34 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
+renderer.xr.enabled = true;
 renderer.setSize( window.innerWidth, window.innerHeight );
+
 document.body.appendChild( renderer.domElement );
+var btnContainer = document.createElement('div');
+var vrButton = VRButton.createButton(renderer);
+var arButton = ARButton.createButton(renderer); 
+vrButton.style.bottom = '70px';
+
+btnContainer.appendChild(vrButton);
+btnContainer.appendChild(arButton);
+document.body.appendChild(btnContainer);
+
 
 new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
 const cube = new THREE.Mesh( geometry, material );
 cube.material.wireframe = true;
 
 scene.add( cube );
 
 camera.position.z = 5;
+
+renderer.setAnimationLoop( function() {
+    renderer.render(scene, camera);
+} );
 
 function animate() {
 	requestAnimationFrame( animate );
