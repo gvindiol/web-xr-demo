3f752bed464e6d1c21de5591a87d66829b0de4cd
Giovanni Vindiola <gvindiol@ucsd.edu>
2023-02-20 03:16:59 -0800

    Added OrbitControls

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 // Creating the scene
 const scene = new THREE.Scene();
@@ -8,9 +9,13 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+new OrbitControls(camera, renderer.domElement)
+
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
+cube.material.wireframe = true;
+
 scene.add( cube );
 
 camera.position.z = 5;
@@ -18,8 +23,8 @@ camera.position.z = 5;
 function animate() {
 	requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01;
+    //cube.rotation.y += 0.01;
     
 	renderer.render( scene, camera );
 }
